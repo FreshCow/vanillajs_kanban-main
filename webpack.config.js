d12d3e4d961c06e3810e682e50f49d541934298d
ssw <ssw8227@gmail.com>
2022-05-15 22:41:09 +0900

    commit

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+
+module.exports = {
+    "mode"   : "none",
+    "entry"  : "./src/app.js",
+    "output" : {
+        "path"    : __dirname + '/dist',
+        "filename": "bundle.js"
+    },
+    devServer: {
+        host: '0.0.0.0',
+        port: 3000,
+        static: path.join(__dirname, 'dist')
+    },
+    "module" : {
+        "rules": [
+            {
+                "test": /\.s[ac]ss$/i,
+                "use" : [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
+            },
+            {
+                "test"   : /\.js$/,
+                "exclude": /node_modules/,
+                "use"    : {
+                    "loader" : "babel-loader",
+                    "options": {
+                        "presets": [
+                            "@babel/preset-env",
+                        ]
+                    }
+                }
+            },
+            {
+                test   : /\.(png|jpe?g|gif)$/i,
+                loader : 'file-loader',
+                options: {
+                    publicPath: './dist/',
+                    name      : '[name].[ext]?[hash]',
+                },
+            },
+        ]
+    }
+};
