a2bd47794f8c05082cb1e1316a481207b503e124
ssw <ssw8227@gmail.com>
2022-05-16 10:06:22 +0900

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     "mode"   : "none",
-    "entry"  : "./src/app.js",
+    "entry"  : "./dist/src/app.js",
     "output" : {
         "path"    : __dirname + '/dist',
         "filename": "bundle.js"
